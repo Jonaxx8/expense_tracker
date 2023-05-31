1473a7203ea2fa4f8eba346491e42a59fccc1f63
Nikil Jonnada <20r21a0543@mlrinstitutions.ac.in>
2023-05-31 11:58:50 +0530

    basic structure achieved

---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,18 @@
+import 'package:app/models/expense.dart';
+import 'package:flutter/widgets.dart';
+
+import 'expenses_item.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expenses});
+
+  final List<Expense> expenses;
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => 
+       ExpenseItem(expenses[index]),
+    );
+  }
+}
